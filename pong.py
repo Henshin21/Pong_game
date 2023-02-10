19e3d881d309ae75456bea88124b448467716313
Tai Cao <Henshin21@yahoo.pl>
2023-02-10 19:02:43 +0100

    added function, keyboard bindings and ball collisions

--- pong.py
+++ pong.py
@@ -31,12 +31,78 @@ ball.shape("square") # forma piłki
 ball.color("white") # kolor piłki
 ball.penup()
 ball.goto(0, 0) #ustawienie piłki na start
+ball.dx = 1
+ball.dy = 1
+
+#Pen
+pen = turtle.Trutle()
+pen.speed(0)
+pen.color("white")
+pen.up()
+pen.hidetrutle()
+pen.goto(0, 260)
+pen.write("Player A: 0  Player B: 0", align= "center")
 
 
 # Funkcje
 def paddle_a_up():
-    y = paddle_a.ycor
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+# Keyboard binding
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
+
 
 # Main game loop
 while True:
     wn.update()
+    
+    # porusznie się piłki
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Sprawdzanie granicy
+    if ball.ycor() > 290: # jeżeli piłki trafi na granic 290 (górny krawężnik)
+        ball.sety(290)
+        ball.dy *= -1     # to piłka odleci w odwróconą stronę
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+    
+    if ball.xcor() > 390: #jeżeli piłka trafi na granice 390 (prawy krawężnik)
+        ball.goto(0, 0)
+        ball.dx *= -1     #to piłka wróci z powrotem na miejsce
+
+    if ball.xcor() < -390: 
+        ball.goto(0, 0)
+        ball.dx *= -1
+
+    #odbicie piłki od paletki
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50):
+        ball.setx(340)
+        ball.dx *= -1
+
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50):
+        ball.setx(-340)
+        ball.dx *= -1
